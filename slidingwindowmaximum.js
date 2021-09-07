50de35b21090649c349f1853277ef39e71b320d1
tarnilok <tarnilok@gmail.com>
2021-09-07 03:10:57 +0300

    slidingwindowmaximum added

---
+++ slidingwindowmaximum.js
@@ -0,0 +1,13 @@
+
+
+// const array = [1,3,-1,-3,5,3,6,7]
+// const k = 3
+// const newarray = []
+// const func = () => {
+//     if (array.length < k) return;
+//     newarray.push(Math.max(...array.slice(0,3)))
+//     array.shift()
+//     func()
+// }
+// func();
+// console.log(newarray)
