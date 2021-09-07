a31b2e95adae7f75fac1954eb4711b3be6e1068a
tarnilok <tarnilok@gmail.com>
2021-09-07 03:58:53 +0300

    new solution added

--- slidingwindowmaximum.js
+++ slidingwindowmaximum.js
@@ -1,13 +1,34 @@
 
-
+// -------------------Solution 1:----------------
 // const array = [1,3,-1,-3,5,3,6,7]
 // const k = 3
 // const newarray = []
 // const func = () => {
 //     if (array.length < k) return;
-//     newarray.push(Math.max(...array.slice(0,3)))
+//     newarray.push(Math.max(...array.slice(0,k)))
 //     array.shift()
 //     func()
 // }
 // func();
 // console.log(newarray)
+
+// ------------------Solution 2----------------
+// const array = [1,3,-1,-3,5,3,6,7]
+// const k = 3
+// const newarray = []
+// const func = () => {
+//     if (array.length < k) return;
+//     newarray.push(Array.from(array.slice(0,k)))
+//     array.shift()
+//     func()
+// }
+// func()
+// // console.log(newarray)
+
+// console.log(newarray.map((arr) => Math.max(...arr)))
+
+
+
+
+
+
