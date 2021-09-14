f8ca7c676adbdb21f39d24d2d95e4bd6f5a6871b
tarnilok <tarnilok@gmail.com>
2021-09-14 12:38:51 +0300

    bracket validator .js is ready

--- bracketValidator.js
+++ bracketValidator.js
@@ -1,6 +1,5 @@
 
 const isVallid  =  (br) => {
-    
     const brackets = {
         "(" : ")",
         "[" : "]",
@@ -9,61 +8,27 @@ const isVallid  =  (br) => {
     const openersBrackets = ["(", "[", "{"];
     const arr = [];
 
-    br.split("").map(e => {
-        if (openersBrackets.includes(e)) {
-            arr.push(e)
-            // console.log(e)
-        }
-        else if (e == brackets[arr[(arr.length)-1]]) {         //empty array and object are true 
-            console.log(arr)
-            arr.pop()
-        }
-        else {return false}
-    });
+  for (const e of br.split("")) {
+    if (openersBrackets.includes(e)) {
+        arr.push(e)
+        // console.log(e)
+    }
+    else if (e === brackets[arr[(arr.length)-1]]) {         //empty array and object are true 
+        // console.log(arr)
+        arr.pop()
+    }
+    else { return false }     // forEach and map doesn't work properly. because they execute a function on every element in an array.     forEach is a function rather than a loop and using break statement gives "syntax error: illegal break statement"
+  }
     // console.log(arr)
-    return arr.length == 0
+    return arr.length === 0
 }
 
-console.log(isVallid(" "))
+// console.log(isVallid("()"))   //true
+// console.log(isVallid("([]{[]})[]{{}()}"))  //true
+// console.log(isVallid("([][]}"))   //false
+// console.log(isVallid("[[]()"))   //false
+// console.log(isVallid("[[]]())"))    //false
+// console.log(isVallid(""))   //true
 
-// const a = "a n k a r a"
-// a.split(" ").forEach(element => {
-//     if (a.includes("k")) console.log("hello") 
-// });
-
-// console.log(a.split(""))
-
-// const a = []
-// a[-1] ? console.log("hello") : console.log("goodbye")
-// a.pop()
-// console.log(a)
-// console.log(a[a.length-1])
-
-// const brackets2 = {
-//     "(" : ")",
-//     "[" : "]",
-//     "{" : "}"
-// };
-
-// console.log("(" == brackets2[undefined])
-
-// const a = () => {
-//     return [] == []
-// }
-
-// console.log(a())
-
-// var brackets = {
-//     "(" : ")",
-//     "[" : "]",
-//     "{" : "}"
-// };
-
-//  const arr = ["[", "{", "("]
-
-//  console.log(brackets[arr[(arr.length)-1]])
-
-// console.log("{[]{()}}".split(""))
-
-// const a = [].length
-// console.log(Boolean(a))
+// console.log(isVallid("..."))
+// console.log(isVallid("()[.{}]"))
